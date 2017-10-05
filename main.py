f011ef7b1f7ca7b269fe79bc80c4b585937cfec1
kevinmk <kevinmk@outlook.com>
2017-10-04 22:53:09 -0500

    committing finished project

--- main.py
+++ main.py
@@ -3,91 +3,45 @@ import cgi
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['DEBUG'] = True      
 
-# --------------------------------------
-terrible_movies = [
-    "Gigli",
-    "Star Wars Episode 1: Attack of the Clones",
-    "Paul Blart: Mall Cop 2",
-    "Nine Lives",
-    "Starship Troopers"
-]
+@app.route("/", methods=['GET', 'POST'])
+def signup():    
+    if request.method == 'GET':
+        return render_template('signup.html')
+    
+    username = request.form['username']
+    password = request.form['password']
+    verifypass = request.form['verifypass']
+    email = request.form['email']
+    username_error=''
+    password_error=''
+    verify_error=''
+    email_error=''
+    count_at = email.count('@')
+    count_period = email.count('.')
 
-def get_current_watchlist():
-    # returns user's current watchlist--hard coded for now
-    return [ "Star Wars", "Minions", "Shawshank Redemption", "My Favorite Martian" ]
+    if username == '' or ' ' in username:
+        username_error = "Username invalid!"
+    elif len(username) < 3 or len(username) > 20:
+        username_error = "Username must be between than 3 and 20 characters!"
+    if password == ''  or ' ' in password:
+        password_error = "Invalid Password!"
+    elif len(password) < 3 or len(password) > 20:
+        password_error = "Password must be between than 3 and 20 characters!"
+    if verifypass != password:
+        verify_error = "Passwords don't match!"
+    if email is not "":
+        if len(email) < 3 or len(email) > 20 or count_at != 1 or count_period != 1 or ' ' in email:
+            email_error = "Invalid Email!"
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect('/welcome?username={0}'.format(username))
+    else:
+        return render_template('signup.html', username=username, email=email, username_error=username_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
 
-# TODO: 
-# Modify "My Watchlist" so that you eliminate the need for the "crossoff" form in edit.html. 
-# Now, next to every list item/movie listed in "My Watchlist" you should display a button that says "I Watched it!". 
-# Clicking the button will result in a confirmation message that the movie has been watched. 
-# So you'll need to add a form within the <li> tags of "My Watchlist"
-# Once this is done, delete the "crossoff" form in edit.html
-
-# TODO:
-# Make a ratings.html template which lists all movies that have been crossed off.
-# It should have a header of <h2>Movies I Have Watched</h2>
-# Add a form for rating EACH list item/movie using a <select> dropdown with the options/values
-# in this list: ["How was it?", "*", "**", "***", "****", "*****"]
-# And with a button that says "Rate It!" to submit the user's rating.
-# Give this form the action of "/rating-confirmation" and the method of post.
-
-# TODO: 
-# Add a function, movie_ratings, to handle a get request and render the template at "/ratings"
-
-# TODO:
-# Add a function, get_watched_movies, to get the list of crossed off movies. 
-# For now, create a hard-coded list with a few movie titles. 
-
-# TODO:
-# Make a rating-confirmation.html template, to be displayed when the user rates a movie 
-# they have crossed off. 
-
-# TODO: 
-# create a rate_movie function that handles a post request on /rating-confirmation and 
-# renders the `rating-confirmation` template.
-
-@app.route("/crossoff", methods=['POST'])
-def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']
-
-    if crossed_off_movie not in get_current_watchlist():
-        # the user tried to cross off a movie that isn't in their list,
-        # so we redirect back to the front page and tell them what went wrong
-        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
-
-        # redirect to homepage, and include error as a query parameter in the URL
-        return redirect("/?error=" + error)
-
-    # if we didn't redirect by now, then all is well
-    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
-
-@app.route("/add", methods=['POST'])
-def add_movie():
-    # look inside the request to figure out what the user typed
-    new_movie = request.form['new-movie']
-
-    # if the user typed nothing at all, redirect and tell them the error
-    if (not new_movie) or (new_movie.strip() == ""):
-        error = "Please specify the movie you want to add."
-        return redirect("/?error=" + error)
-
-    # if the user wants to add a terrible movie, redirect and tell them the error
-    if new_movie in terrible_movies:
-        error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
-        return redirect("/?error=" + error)
-
-    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    new_movie_escaped = cgi.escape(new_movie, quote=True)
-
-    return render_template('add-confirmation.html', movie=new_movie)
-
-
-@app.route("/")
-def index():
-    encoded_error = request.args.get("error")
-    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+@app.route("/welcome")
+def welcome(): 
+    username = request.args.get('username')
+    return render_template('welcome.html', username=username)
 
 app.run()
-
